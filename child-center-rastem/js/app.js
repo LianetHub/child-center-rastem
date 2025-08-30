6f7c87c70b0a2108ebacece9b250e976768ee386
lianet <jack.tolmak@yandex.ru>
2025-08-30 14:34:02 +0300

    add init commit

---
+++ child-center-rastem/js/app.js
@@ -0,0 +1,31 @@
+"use strict";
+
+//  Fancybox
+if (typeof Fancybox !== "undefined" && Fancybox !== null) {
+    Fancybox.bind("[data-fancybox]", {
+        dragToClose: false
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // header animation
+
+    const header = document.querySelector('.header');
+
+    window.addEventListener('wheel', (e) => {
+        if (e.deltaY > 0) {
+            header.classList.add('header--hidden');
+        } else {
+            header.classList.remove('header--hidden');
+        }
+    });
+
+
+    document.addEventListener('click', (e) => {
+        const target = e.target;
+
+    });
+
+
+});
